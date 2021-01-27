518120f5cfe9eeaf63d7d710a0ea22378216d885
harispervaiz <harispervaiz2@hotmail.com>
2021-01-27 19:07:59 +0100

    api

--- fognode/index.php
+++ fognode/index.php
@@ -1,11 +1,31 @@
 <?php
+$data = array();
 
-$data = array(
-    'name' => 'sensor1',
-    'temperature' => 5 ,
-    'datetime' => '2021-01-27 19:00:46',
-    'status' => 1,
-);
+
+$data[] = array('name' => 'sensor1',
+        'temperature' => 5 ,
+        'datetime' => '2021-01-27 19:00:46',
+        'status' => 1,
+        );
+$data[] = array('name' => 'sensor1',
+        'temperature' => 6 ,
+        'datetime' => '2021-01-27 19:02:46',
+        'status' => 1,
+        );
+
+$data[] = array('name' => 'sensor2',
+        'temperature' => 7 ,
+        'datetime' => '2021-01-27 19:01:46',
+        'status' => 1,
+        );
+
+$data[] = array('name' => 'sensor2',
+        'temperature' => 6 ,
+        'datetime' => '2021-01-27 19:05:46',
+        'status' => 1,
+        );
+
+
 header('Content-Type: application/json');
 echo json_encode($data);
 
