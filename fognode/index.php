bc3e47922f8a83739c238209d8bebe324c5eb3e5
harispervaiz <harispervaiz2@hotmail.com>
2021-01-27 19:02:05 +0100

    static data

--- fognode/index.php
+++ fognode/index.php
@@ -1,10 +1,19 @@
 <?php
 
-$ch = curl_init();
+$data = array(
+    'name' => 'sensor1',
+    'temperature' => 5 ,
+    'datetime' => '2021-01-27 19:00:46',
+    'status' => 1,
+);
+header('Content-Type: application/json');
+echo json_encode($data);
+
+/*$ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "http://127.0.0.1/fog_computing2021/sensors/temp1.php");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $result = curl_exec($ch);
 
 
 print_r($result);
-curl_close($ch);
+curl_close($ch);*/
