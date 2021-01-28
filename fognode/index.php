3921a25c89b62287e59f9acdb2cb2da97399ebe8
harispervaiz <harispervaiz2@hotmail.com>
2021-01-28 12:05:11 +0100

    update

--- fognode/index.php
+++ fognode/index.php
@@ -1,39 +1,20 @@
 <?php
+
+include_once __DIR__ . '/../database/connection.php';
+
+$sql = "select * from data";
+$query = mysqli_query($db_conx, $sql) or die("Error at query " . $sql . '-- ' . mysqli_errno($db_conx));;
 $data = array();
-
-
-$data[] = array('name' => 'sensor1',
-        'temperature' => 5 ,
-        'datetime' => '2021-01-27 19:00:46',
-        'status' => 1,
+if(mysqli_num_rows($query) > 0){ 
+while($row=mysqli_fetch_assoc($query)){
+    $data[] =  array(
+        'name' => $row['name'],
+        'temperature' =>$row['temperature'],
+        'datetime' => $row['datetime'],
+        'status' => $row['status'],
         );
-$data[] = array('name' => 'sensor1',
-        'temperature' => 6 ,
-        'datetime' => '2021-01-27 19:02:46',
-        'status' => 1,
-        );
-
-$data[] = array('name' => 'sensor2',
-        'temperature' => 7 ,
-        'datetime' => '2021-01-27 19:01:46',
-        'status' => 1,
-        );
-
-$data[] = array('name' => 'sensor2',
-        'temperature' => 6 ,
-        'datetime' => '2021-01-27 19:05:46',
-        'status' => 1,
-        );
-
+}
+}
 
 header('Content-Type: application/json');
 echo json_encode($data);
-
-/*$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "http://127.0.0.1/fog_computing2021/sensors/temp1.php");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-$result = curl_exec($ch);
-
-
-print_r($result);
-curl_close($ch);*/
---
+++ sensors/update.php
@@ -0,0 +1,21 @@
+<?php
+
+include_once __DIR__ . '/../database/connection.php';
+
+if (isset($_POST['temperature']) && isset($_POST['datetime']) && isset($_POST['status']) && isset($_POST['name'])) {
+    $temperature = $_POST['temperature'];
+    $datetime = $_POST['datetime'];
+    $status = $_POST['status'];
+    $name = $_POST['name'];
+
+    $sql="INSERT INTO `data`(`temperature`, `datetime`,`status`,`name`) VALUES ('$temperature','$datetime','$status','$name') ";
+    $query=mysqli_query($db_conx,$sql) or die("Error at query " . $sql . '-- ' . mysqli_errno($db_conx));
+
+    if(mysqli_affected_rows($db_conx)){
+
+        echo "success";
+        exit();
+
+    }
+
+}
