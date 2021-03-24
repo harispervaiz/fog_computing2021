613183cf05ff41ca1f6c09ec2ba665b01fbbf662
Haris <harispervaiz2@hotmail.com>
2021-03-24 18:07:10 +0100

    Update connection.php

--- database/connection.php
+++ database/connection.php
@@ -1,5 +1,5 @@
 <?php
-$db_conx = mysqli_connect("localhost", "root", "h@z!@#", "fog_computing");
+$db_conx = mysqli_connect("DB_HOST", "DB_USERNAME", "DB_PASSWORD", "DB_NAME");
 // Evaluate the connection
 if (mysqli_connect_errno()) {
     echo mysqli_connect_error();
